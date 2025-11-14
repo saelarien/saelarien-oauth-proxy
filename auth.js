6cf9a8c661e4aece0b6f8e65e4b54b05b017cd1b
Saelariën <saelariensnack420@gmail.com>
2025-11-13 22:36:47 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,9 +3,9 @@ const randomstring = require('randomstring')
 module.exports = (oauth2) => {
   // Authorization uri definition
   const authorizationUri = oauth2.authorizeURL({
-    redirectURI: process.env.REDIRECT_URL,
-    scope: process.env.SCOPES || 'repo,user',
-    state: randomstring.generate(32)
+  redirectURI: process.env.REDIRECT_URL,
+  scope: process.env.SCOPES || 'repo,user',
+  state: randomstring.generate(32)
   })
 
   return (req, res, next) => {
